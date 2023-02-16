4ea4429ed682ef37c572633131fcc459c1bdaa84
Crashprn <thewall907@gmail.com>
2023-02-16 15:50:42 -0700

    added seperate uuv launch file

---
+++ ros_ws/src/nodes_py/launch/arduino_joy_uuv.launch.py
@@ -0,0 +1,16 @@
+from launch import LaunchDescription
+from launch_ros.actions import Node
+
+def generate_launch_description():
+    return LaunchDescription([
+        Node(
+            package='nodes_py',
+            executable='arduino_interface',
+            name="arduino_interface"
+        ),
+        Node(
+            package="joy_linux",
+            executable='joy_linux_node',
+            name="joy_node",
+        )
+    ])
